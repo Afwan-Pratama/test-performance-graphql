0f465c0c5da4bba0915c6a9667d3eb8facb0f071
Afwan-Pratama <phil.pratama@gmail.com>
2023-01-25 16:52:20 +0700

    fix : clause where from OR to AND on query filtering product

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import { gql, useLazyQuery } from '@apollo/client'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
